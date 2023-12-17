ba11d20cd00b442c1f51f5ff3f74b46ab5342aac
thomas <jonestho@mail.gvsu.edu>
2023-12-16 23:16:23 -0500

    first version of home screen

--- StudioGhibliDiscovery/UCMain.Designer.cs
+++ StudioGhibliDiscovery/UCMain.Designer.cs
@@ -28,24 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.viewFilms = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.viewPeople = new System.Windows.Forms.Button();
+            this.viewLocations = new System.Windows.Forms.Button();
+            this.viewSpecies = new System.Windows.Forms.Button();
+            this.viewVehicles = new System.Windows.Forms.Button();
+            this.viewFavorites = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(80, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(888, 109);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Welcome to the Studio Ghibli Discovery!";
+            // 
+            // viewFilms
+            // 
+            this.viewFilms.BackColor = System.Drawing.Color.Black;
+            this.viewFilms.Font = new System.Drawing.Font("Javanese Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewFilms.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewFilms.Location = new System.Drawing.Point(404, 170);
+            this.viewFilms.Name = "viewFilms";
+            this.viewFilms.Size = new System.Drawing.Size(176, 75);
+            this.viewFilms.TabIndex = 2;
+            this.viewFilms.Text = " Films";
+            this.viewFilms.UseVisualStyleBackColor = false;
+            this.viewFilms.Click += new System.EventHandler(this.viewFilms_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(203, 291);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(263, 87);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(619, 128);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Home Screen";
+            this.label1.Size = new System.Drawing.Size(482, 80);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "What would you like to view?";
+            // 
+            // viewPeople
+            // 
+            this.viewPeople.BackColor = System.Drawing.Color.Black;
+            this.viewPeople.Font = new System.Drawing.Font("Javanese Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewPeople.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewPeople.Location = new System.Drawing.Point(404, 257);
+            this.viewPeople.Name = "viewPeople";
+            this.viewPeople.Size = new System.Drawing.Size(176, 75);
+            this.viewPeople.TabIndex = 4;
+            this.viewPeople.Text = "People";
+            this.viewPeople.UseVisualStyleBackColor = false;
+            this.viewPeople.Click += new System.EventHandler(this.viewPeople_Click);
+            // 
+            // viewLocations
+            // 
+            this.viewLocations.BackColor = System.Drawing.Color.Black;
+            this.viewLocations.Font = new System.Drawing.Font("Javanese Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewLocations.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewLocations.Location = new System.Drawing.Point(372, 343);
+            this.viewLocations.Name = "viewLocations";
+            this.viewLocations.Size = new System.Drawing.Size(245, 75);
+            this.viewLocations.TabIndex = 5;
+            this.viewLocations.Text = "Locations";
+            this.viewLocations.UseVisualStyleBackColor = false;
+            this.viewLocations.Click += new System.EventHandler(this.viewLocations_Click);
+            // 
+            // viewSpecies
+            // 
+            this.viewSpecies.BackColor = System.Drawing.Color.Black;
+            this.viewSpecies.Font = new System.Drawing.Font("Javanese Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewSpecies.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewSpecies.Location = new System.Drawing.Point(404, 520);
+            this.viewSpecies.Name = "viewSpecies";
+            this.viewSpecies.Size = new System.Drawing.Size(176, 75);
+            this.viewSpecies.TabIndex = 6;
+            this.viewSpecies.Text = "Species";
+            this.viewSpecies.UseVisualStyleBackColor = false;
+            this.viewSpecies.Click += new System.EventHandler(this.viewSpecies_Click);
+            // 
+            // viewVehicles
+            // 
+            this.viewVehicles.BackColor = System.Drawing.Color.Black;
+            this.viewVehicles.Font = new System.Drawing.Font("Javanese Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewVehicles.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewVehicles.Location = new System.Drawing.Point(380, 430);
+            this.viewVehicles.Name = "viewVehicles";
+            this.viewVehicles.Size = new System.Drawing.Size(227, 78);
+            this.viewVehicles.TabIndex = 7;
+            this.viewVehicles.Text = "Vehicles";
+            this.viewVehicles.UseVisualStyleBackColor = false;
+            this.viewVehicles.Click += new System.EventHandler(this.viewVehicles_Click);
+            // 
+            // viewFavorites
+            // 
+            this.viewFavorites.BackColor = System.Drawing.Color.Black;
+            this.viewFavorites.Font = new System.Drawing.Font("Javanese Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewFavorites.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewFavorites.Location = new System.Drawing.Point(357, 639);
+            this.viewFavorites.Name = "viewFavorites";
+            this.viewFavorites.Size = new System.Drawing.Size(267, 75);
+            this.viewFavorites.TabIndex = 8;
+            this.viewFavorites.Text = "My Favorites";
+            this.viewFavorites.UseVisualStyleBackColor = false;
             // 
             // UCMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StudioGhibliDiscovery.Properties.Resources.Background;
+            this.Controls.Add(this.viewFavorites);
+            this.Controls.Add(this.viewVehicles);
+            this.Controls.Add(this.viewSpecies);
+            this.Controls.Add(this.viewLocations);
+            this.Controls.Add(this.viewPeople);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.viewFilms);
+            this.Controls.Add(this.label2);
             this.Name = "UCMain";
             this.Size = new System.Drawing.Size(1024, 768);
             this.ResumeLayout(false);
@@ -54,7 +160,13 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button viewFilms;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button viewPeople;
+        private System.Windows.Forms.Button viewLocations;
+        private System.Windows.Forms.Button viewSpecies;
+        private System.Windows.Forms.Button viewVehicles;
+        private System.Windows.Forms.Button viewFavorites;
     }
 }
--- StudioGhibliDiscovery/UCMain.cs
+++ StudioGhibliDiscovery/UCMain.cs
@@ -16,5 +16,30 @@ namespace StudioGhibliDiscovery
         {
             InitializeComponent();
         }
+
+        private void viewLocations_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewSpecies_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewFilms_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewVehicles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewPeople_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
